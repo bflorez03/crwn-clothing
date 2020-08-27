56f8ed45aaf92871ed0a50636126b8a11d7a6695
Brayan Florez <bflorez@outlook.com>
2020-08-27 19:01:55 +0200

    move directory and shop data into its own reducer and update corresponding component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,18 +1,14 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  jackets: 2,
-  sneakers: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([selectShop], (shop) => shop.collections);
 
+export const selectCollectionsForPreview = createSelector([selectCollections], (collections) =>
+  Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectCollections], (collections) => collections.find((c) => c.id === COLLECTION_ID_MAP[collectionUrlParam]))
+  createSelector([selectCollections], (collections) => collections[collectionUrlParam])
 );
