382541734315c57fc809c57dd5187f8495a33cb9
Brayan Florez <bflorez@outlook.com>
2021-01-31 20:15:37 +0100

    stripe payment button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51HLWngIiLpIs0KA160JtMv7hQ3jYQZpfIed8t2UvvudQzOpptS9ibQV8dTqoRv9EbQkKEAEezggQu2Q96Utc5AZX00SqnGlqZz';
 
-  const onToken = (token) => {
-    console.log(token);
-    alert('Payment successful');
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then(response => {
+        alert('Payment successful');
+      })
+      .catch(error => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert('There was an issue with your payment, Please sure you use the provided credit cart');
+      });
   };
 
   return (
